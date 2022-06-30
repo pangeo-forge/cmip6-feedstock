a8a33f48085ae573f5e41228ac5dbac243feb96c
Julius Busecke <julius@ldeo.columbia.edu>
2022-06-30 16:11:43 -0400

    Try out PMIP request

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -455,6 +455,7 @@ iids = [
     "CMIP6.DAMIP.NCC.NorESM2-LM.hist-aer.r2i1p1f1.Amon.pr.gn.v20190920",
     "CMIP6.DAMIP.NCC.NorESM2-LM.hist-aer.r3i1p1f1.Amon.pr.gn.v20190920",
     "CMIP6.DAMIP.NOAA-GFDL.GFDL-ESM4.hist-aer.r1i1p1f1.Amon.pr.gr1.v20180701",
+    "CMIP6.PMIP.MPI-M.MPI-ESM1-2-LR.past2k.r1i1p1f1.Amon.tas.gn"
 ]
 # TODO: should implement a retry + backoff (i have seen flaky datasets come back after a few minutes.
 
