0c6e14d061504c5d0a3ca6dd2e1c34945bf072da
Julius Busecke <julius@ldeo.columbia.edu>
2022-07-14 16:14:26 +0000

    clean up recipe

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -453,15 +453,12 @@ iids = [
     "CMIP6.DAMIP.NCC.NorESM2-LM.hist-aer.r2i1p1f1.Amon.pr.gn.v20190920",
     "CMIP6.DAMIP.NCC.NorESM2-LM.hist-aer.r3i1p1f1.Amon.pr.gn.v20190920",
     "CMIP6.DAMIP.NOAA-GFDL.GFDL-ESM4.hist-aer.r1i1p1f1.Amon.pr.gr1.v20180701",
+    #PMIP runs requested by @CommonClimate
+    'CMIP6.PMIP.MIROC.MIROC-ES2L.past1000.r1i1p1f2.Amon.tas.gn.v20200318',
+    'CMIP6.PMIP.MRI.MRI-ESM2-0.past1000.r1i1p1f1.Amon.tas.gn.v20200120',
+    'CMIP6.PMIP.MPI-M.MPI-ESM1-2-LR.past2k.r1i1p1f1.Amon.tas.gn.v20210714',
 ]
 # TODO: should implement a retry + backoff (i have seen flaky datasets come back after a few minutes.
-
-# just for testing the PMIP data overwrite the list
-iids = [
-    "CMIP6.PMIP.MPI-M.MPI-ESM1-2-LR.past2k.r1i1p1f1.Amon.tas.gn.dummy",
-    "CMIP6.PMIP.MRI.MRI-ESM2-0.past1000.r1i1p1f1.Amon.tas.gn.dummy",
-    "CMIP6.PMIP.MIROC.MIROC-ES2L.past1000.r1i1p1f2.Amon.tas.gn.dummy",
-]
 
 # Lets try to implement retrys
 async def main(node_list):
